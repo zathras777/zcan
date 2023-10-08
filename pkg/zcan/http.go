46fc87fb3d3662197f5a8cb77e687e37c35dca0c
david reid <zathrasorama@gmail.com>
2023-10-08 18:10:13 +0800

    Add device info endpoint Simplify device data

--- pkg/zcan/http.go
+++ pkg/zcan/http.go
@@ -11,6 +11,7 @@ func (dev *ZehnderDevice) startHttpServer(host string, port int) {
 	log.Printf("Starting HTTP server listening @ http://%s:%d/", host, port)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", dev.jsonResponse)
+	mux.HandleFunc("/device-info", dev.jsonDeviceInfo)
 	mux.HandleFunc("/dump", dev.dumpPDO)
 
 	dev.http = &http.Server{Addr: fmt.Sprintf("%s:%d", host, port), Handler: mux}
@@ -27,8 +28,6 @@ func (dev *ZehnderDevice) startHttpServer(host string, port int) {
 func (dev *ZehnderDevice) jsonResponse(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	dataMap := make(map[string]interface{})
-	dataMap["model"] = dev.Model
-	dataMap["serial_number"] = dev.SerialNumber
 
 	for _, v := range dev.pdoData {
 		dataMap[v.Sensor.slug] = v.GetData()
@@ -39,7 +38,28 @@ func (dev *ZehnderDevice) jsonResponse(w http.ResponseWriter, r *http.Request) {
 		w.Write(outData)
 		return
 	}
-	log.Printf("Unable to generate json data: %s", err)
+	log.Printf("jsonResponse: Unable to generate json data: %s", err)
+}
+
+func (dev *ZehnderDevice) jsonDeviceInfo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	log.Printf("dev.SerialNumber: %s", dev.SerialNumber)
+	if dev.SerialNumber == "" {
+		syncer := make(chan bool)
+		dev.getDeviceInfo(syncer)
+		<-syncer
+	}
+	dataMap := make(map[string]interface{})
+	dataMap["model"] = dev.Model
+	dataMap["serial_number"] = dev.SerialNumber
+	dataMap["software_version"] = dev.SoftwareVersion
+
+	outData, err := json.Marshal(dataMap)
+	if err == nil {
+		w.Write(outData)
+		return
+	}
+	log.Printf("jsonDeviceInfo: Unable to generate json data: %s", err)
 }
 
 func (dev *ZehnderDevice) dumpPDO(w http.ResponseWriter, r *http.Request) {
